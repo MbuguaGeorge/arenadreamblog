cae7c71fb017daa212da50ca5e2d2bfeb2be1870
titusowuor30 <titusowuor30@gmail.com>
2021-07-13 16:36:35 +0000

    updstes

--- posts/forms.py
+++ posts/forms.py
@@ -7,24 +7,22 @@ from .models import *
 
 class TinyMCEWidget(TinyMCE):
     def use_required_attribute(self, *args):
-        return False
+        return True
 
 
 class PostForm(forms.ModelForm):
-    content = forms.CharField(
-        widget=TinyMCEWidget(
-            attrs={'required': False, 'cols': 30, 'rows': 10}
-        )
-    )
+    content = forms.CharField(widget=TinyMCEWidget( attrs={'required': False, 'cols': 30, 'rows': 10}) )
+    overview= forms.CharField(widget=TinyMCEWidget(attrs={'required': False, 'cols': 25, 'rows': 10}))
 
     class Meta:
         model = Post
-        fields = ('title', 'overview', 'content', 'thumbnail', 
+        fields = ('title', 'overview', 'content', 'thumbnail',
         'categories', 'featured', 'previous_post', 'next_post')
 
 
 class CommentForm(forms.ModelForm):
-    content = forms.CharField(widget=forms.Textarea(attrs={
+
+    content = forms.CharField(widget=forms.textarea(attrs={
         'class': 'form-control',
         'placeholder': 'Type your comment',
         'id': 'usercomment',
